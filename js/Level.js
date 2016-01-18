507c5c48db27a202797951f966a79178d576eccd
Niek <niek_vanhulzen@hotmail.com>
2016-01-18 17:29:59 +0100

    leveleditor updated + Levels

--- js/Level.js
+++ js/Level.js
@@ -1,6 +1,6 @@
 function Level()
 {
-	this.gallery = {};
+	this.gallery = new PIXI.Polygon([]);
 	this.holes = [];
 	this.obstacles = [];
 	this.covers = [];
@@ -11,13 +11,29 @@ function Level()
 
 Level.prototype.constructor = Level;
 
-Level.prototype.load = function()
+Level.prototype.load = function(level)
 {
-
+	newlevel = JSON.parse(level);
+	this.gallery.points = newlevel.gallery.points;
+	for (var i = 0; i < newlevel.holes.length; i++) {
+		this.holes.push(new PIXI.Polygon(newlevel.holes[i].points));
+	};
+	for (var i = 0; i < newlevel.obstacles.length; i++) {
+		this.obstacles.push(new PIXI.Polygon(newlevel.obstacles[i].points));
+	};
+	for (var i = 0; i < newlevel.covers.length; i++) {
+		this.covers.push(new PIXI.Polygon(newlevel.covers[i].points));
+	};
+	this.paintings = newlevel.paintings;
+	this.player = newlevel.player;
+	for (var i = 0; i < newlevel.guards.length; i++) {
+		this.guards.push({position : newlevel.guards[i].position,
+							guardpath : new PIXI.Polygon(newlevel.guards[i].guardpath.points)});
+	};
 }
 
-Level.prototype.save  = function()
+Level.prototype.save = function()
 {
-
+	return JSON.stringify(this);
 }
 
--- js/leveleditor.js
+++ js/leveleditor.js
@@ -10,137 +10,106 @@ var floorSprite = new PIXI.extras.TilingSprite(texture, 1280, 720);
 // Enable/Disable debugging
 var debug = false;
 
-var level = {
-	gallery : new PIXI.Polygon([]),
-	holes : [],
-	obstacles : [],
-	paintings : [],
-	guards : [],
-	player : new PIXI.Point()
-};
-		// Gallery polygon
-		//new PIXI.Polygon//([
-		// 	new PIXI.Point(50,20),
-		// 	new PIXI.Point(700,140),
-		// 	new PIXI.Point(1200,150),
-		// 	new PIXI.Point(1000,585),
-		// 	new PIXI.Point(830,605),
-		// 	new PIXI.Point(700,350),
-		// 	new PIXI.Point(500,600),
-		// 	new PIXI.Point(470,540),
-		// 	new PIXI.Point(100,650),
-		// 	new PIXI.Point(140,230)
-		// ]),
-		// // Holes
-		// new PIXI.Polygon([
-		// 	new PIXI.Point(290,250),
-		// 	new PIXI.Point(400,230),
-		// 	new PIXI.Point(450,350),
-		// 	new PIXI.Point(300,420)]
-		// ),
-		// new PIXI.Polygon([
-		// 	new PIXI.Point(570,200),
-		// 	new PIXI.Point(680,260),
-		// 	new PIXI.Point(530,340)]
-		// ),
-		// new PIXI.Polygon([
-		// 	new PIXI.Point(850,250),
-		// 	new PIXI.Point(940,310),
-		// 	new PIXI.Point(780,340),
-		// 	new PIXI.Point(750,310)]
-		// )
-
-var guardpath = [
-	new PIXI.Point(500,250),
-	new PIXI.Point(350,150),
-	new PIXI.Point(250,150),
-	new PIXI.Point(700,140),
-	new PIXI.Point(820,190),
-	new PIXI.Point(700,140),
-	new PIXI.Point(500,150)
-];
-
-var paintings = [
-	{
-		pos: new PIXI.Point(400,100),
-		value: 1000
-	},
-	{
-		pos: new PIXI.Point(600,100),
-		value: 500
-	}
-];
-
-var visibilityPolygon;
-
-//create graphics
-var galleryMask = new PIXI.Graphics();
-galleryMask.beginFill(0);
-galleryMask.drawPolygon(level.gallery.points);
-galleryMask.endFill();
-
-var visibilityMask = new PIXI.Graphics();
-stage.addChild(visibilityMask);
-
+//initialise
 var mousedown = false;
+var snapPoints = false;
 
-stage.addChild(galleryMask);
-stage.addChild(floorSprite);
+//select option variables
+var creategallery = false;
+var newHole = false;
+var createguards = false;
+var createpath = false;
+var createplayer = false;
 
-var galleryGraphics = new PIXI.Graphics();
-for (var i = level.holes.length - 1; i >= 1; i--) {
-	galleryGraphics.beginFill(0xCCCCCC);
-	galleryGraphics.drawPolygon(level.holes.points);
-	galleryGraphics.endFill();
-	galleryGraphics.lineStyle(5, 0xFFFFFF, 1);
-	galleryGraphics.drawPolygon(level.holes.points);
-};
-stage.addChild(galleryGraphics);
-
-var wallgraphics = new PIXI.Graphics();
-wallgraphics.lineStyle(5, 0xFFFFFF, 1);
-wallgraphics.drawPolygon(level.gallery.points);
-stage.addChild(wallgraphics);
-
-var pointgraphics = new PIXI.Graphics();
-pointgraphics.lineStyle(1, 0x000000, 1);
-pointgraphics.beginFill(0x000000);
-
-// Draw gallery points
-for (var i = 0; i < level.gallery.points.length; i+=2) {
-	pointgraphics.drawCircle(level.gallery[i], level.gallery[i+1], 5);
-};
-
-pointgraphics.endFill();
-
-stage.addChild(pointgraphics);
+//drawscene variables
+var pointarray = [];
+var pointselected = -1;
+var holeselected = -1;
+var guardselected = -1;
 
 var starttime;
 var lastframe;
 
+var level = new Level();
+
+//create graphics
+var galleryMask = new PIXI.Graphics();
+var patrolGraphics = new PIXI.Graphics();
+var galleryGraphics = new PIXI.Graphics();
+var wallgraphics = new PIXI.Graphics();
+var pointgraphics = new PIXI.Graphics();
 var guardGraphics = new PIXI.Graphics();
-guardGraphics.lineStyle(1, 0x000000, 1);
-guardGraphics.beginFill(0xff0000);
-for (var i = 0; i < level.guards.length; i++) {
-	guardGraphics.drawCircle(level.guards[i].position.x, level.guards[i].position.y, 10);
-};
 
-guardGraphics.endFill();
+galleryMask.beginFill(0);
+galleryMask.drawPolygon(level.gallery.points);
+galleryMask.endFill();
 
+stage.addChild(galleryMask);
+stage.addChild(floorSprite);
+stage.addChild(galleryGraphics);
+stage.addChild(patrolGraphics);
+stage.addChild(wallgraphics);
+stage.addChild(pointgraphics);
 stage.addChild(guardGraphics);
-
-var triangleGraphics = new PIXI.Graphics();
-stage.addChild(triangleGraphics);
 
 floorSprite.mask = galleryMask;
 
 stage.interactive = true;
+stage.buttonMode = true;
 stage.on("mousemove", mouseEventHandler);
 stage.on("mousedown", mouseEventHandler);
 stage.on("mouseup", mouseEventHandler);
-
+var ctrlKey = keyboard(16);
 
 update();
+
+function keyboard(keyCode) 
+{
+  var key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = function(event) 
+  {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
+
+  //The `upHandler`
+  key.upHandler = function(event) 
+  {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
+
+  //Attach event listeners
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
+  return key;
+}
+  ctrlKey.press = function() 
+  {
+    snapPoints = true;
+  };
+  ctrlKey.release = function() 
+  {
+    snapPoints = false;
+  };
 
 function update()
 {   
@@ -151,15 +120,10 @@ function update()
     renderer.render(stage);
  }
 
-var creategallery = false;
-var newHole = false;
-var createguards = false;
-var createpath = false;
-var createplayer = false;
-
 function CreateScene(scene)
 {
 	pointarray = [];
+	guardselected = -1;
 	creategallery = false;
 	newHole = false;
 	createguards = false;
@@ -194,15 +158,18 @@ function CreateScene(scene)
 			break;
 	}
 }
+
 function Export()
 {
 	tb_Export.value = JSON.stringify(level);
 }
 
-var pointarray = [];
-var pointselected = -1;
-var holeselected = -1;
-var guardselected = -1;
+function Import()
+{
+	level.load(document.getElementById('tb_Export').value);
+	redraw();
+}
+
 function mouseEventHandler(event)
 {
 	var position = event.data.global;
@@ -252,15 +219,38 @@ function mouseEventHandler(event)
 			if (level.guards[i].position.x <= position.x + 10 && level.guards[i].position.x >= position.x - 10 && 
 				level.guards[i].position.y <= position.y + 10 && level.guards[i].position.y >= position.y - 10)
 			{
+				guardselected = i;
 				pointarray = [];
+				if(createpath)
+				{
+					pointarray = level.guards[i].guardpath.points;
+					pointselected = 0;
+					break;
+				}
 				creategallery = false;
 				createguards = true;
 				newHole = false;
 				createplayer = false;
-				guardselected = i;
 			}
 		};
 
+		if((createpath || createguards) && guardselected >= 0)
+		{
+			for (var j = 0; j < level.guards[guardselected].guardpath.points.length; j+=2) {
+				console.log(level.guards[guardselected].guardpath)
+				if (level.guards[guardselected].guardpath.points[j] <= position.x + 10 && level.guards[guardselected].guardpath.points[j] >= position.x - 10 && 
+					level.guards[guardselected].guardpath.points[j+1] <= position.y + 10 && level.guards[guardselected].guardpath.points[j+1] >= position.y - 10)
+				{
+					pointarray = level.guards[guardselected].guardpath.points;
+					createpath = true;
+					creategallery = false;
+					createguards = false;
+					newHole = false;
+					createplayer = false;
+					pointselected = j;
+				}
+			};
+		}
 		//Check whether player is selected
 		if (level.player.x <= position.x + 10 && level.player.x >= position.x - 10 && 
 			level.player.y <= position.y + 10 && level.player.y >= position.y - 10)
@@ -279,8 +269,11 @@ function mouseEventHandler(event)
 
 		if(createguards && guardselected < 0 && level.gallery.contains(position.x, position.y)) //Create new Guard
 		{
-			level.guards.push({position: new PIXI.Point(Math.floor(position.x), Math.floor(position.y))});
+			level.guards.push({position: new PIXI.Point(Math.floor(position.x), Math.floor(position.y)),
+								guardpath: new PIXI.Polygon([])});
+			guardselected = level.guards.length - 1;
 		}
+
 
 		mousedown = true;
 		var jsonstring = JSON.stringify(pointarray);
@@ -292,28 +285,41 @@ function mouseEventHandler(event)
 	{
 		console.log("up : " + position.x + "h : " + position.y);
 		pointselected = -1;
-		guardselected = -1;
+		if(!createpath)
+			guardselected = -1;
 		mousedown = false;
 	}
 	
 	if(mousedown)
 	{
-		if(creategallery)
+		if((creategallery || newHole) && snapPoints)
 		{
-			console.log("check");
+			//Snap with gallery walls
 			for (var i = 0; i < level.gallery.points.length; i+=2) {
-				console.log("check22");
-				if(pointselected < 0 && i >= pointarray.length - 2)
+				if(pointselected < 0 && (i >= pointarray.length - 2 && creategallery))
 					break;
-				if (level.gallery.points[i] <= position.x + 20 && level.gallery.points[i] >= position.x - 20 && i != pointselected)
+				if (level.gallery.points[i] <= position.x + 20 && level.gallery.points[i] >= position.x - 20 && (i != pointselected || !creategallery))
 				{
-					console.log("xaxis");
 					position.x = level.gallery.points[i];
 				}
-				if (level.gallery.points[i+1] <= position.y + 20 && level.gallery.points[i+1] >= position.y - 20 && i != pointselected)
+				if (level.gallery.points[i+1] <= position.y + 20 && level.gallery.points[i+1] >= position.y - 20 && (i != pointselected || !creategallery))
 				{
-					console.log("yaxis");
 					position.y = level.gallery.points[i+1];
+				}
+			};
+			//Snap with hole walls
+			for (var j = 0; j < level.holes.length; j++) {
+				for (var i = 0; i < level.holes[j].points.length; i+=2) {
+					if(pointselected < 0 && (i >= pointarray.length - 2 && newHole))
+						break;
+					if (level.holes[j].points[i] <= position.x + 20 && level.holes[j].points[i] >= position.x - 20 && (i != pointselected || !newHole))
+					{
+						position.x = level.holes[j].points[i];
+					}
+					if (level.holes[j].points[i+1] <= position.y + 20 && level.holes[j].points[i+1] >= position.y - 20 && (i != pointselected || !newHole))
+					{
+						position.y = level.holes[j].points[i+1];
+					}
 				}
 			};
 		}
@@ -340,9 +346,20 @@ function mouseEventHandler(event)
 		else if(newHole && holeselected >= 0){ // change current hole point position
 			level.holes[holeselected] = new PIXI.Polygon(pointarray);
 		}
-		else if(guardselected >= 0 && level.gallery.contains(position.x, position.y)){ //Change current guard position
+		else if(guardselected >= 0 && !createpath && level.gallery.contains(position.x, position.y)){ //Change current guard position
+			console.log("new position");
 			level.guards[guardselected].position.x = Math.floor(position.x);
 			level.guards[guardselected].position.y = Math.floor(position.y);
+			level.guards[guardselected].guardpath.points[0] = Math.floor(position.x);
+			level.guards[guardselected].guardpath.points[1] = Math.floor(position.y);
+			console.log(level.guards[guardselected].guardpath);
+		}
+		else if(guardselected >= 0 && createpath && level.gallery.contains(position.x, position.y)){
+			console.log("new path");
+			level.guards[guardselected].position.x = Math.floor(pointarray[0]);
+			level.guards[guardselected].position.y = Math.floor(pointarray[1]);
+			level.guards[guardselected].guardpath = new PIXI.Polygon(pointarray);
+			console.log(level.guards[guardselected].guardpath);
 		}
 		else if(createplayer && level.gallery.contains(position.x, position.y)){ //Change player position
 			var candraw = true;
@@ -353,53 +370,69 @@ function mouseEventHandler(event)
 			if(candraw)
 				level.player = new PIXI.Point(Math.floor(position.x), Math.floor(position.y));
 		}
-
-		//Clear all graphics
-		galleryMask.clear();
-		wallgraphics.clear();
-		pointgraphics.clear();
-		galleryGraphics.clear();
-		guardGraphics.clear();
-
-		//draw Gallery
-		galleryMask.beginFill(0);
-		galleryMask.drawPolygon(level.gallery.points);
-		galleryMask.endFill();
-		wallgraphics.lineStyle(5, 0xFFFFFF, 1);
-		wallgraphics.drawPolygon(level.gallery.points);
-
-		//draw Holes
-		for (var i = 0; i < level.holes.length; i++) {
-			galleryGraphics.beginFill(0xCCCCCC);
-			galleryGraphics.drawPolygon(level.holes[i].points);
-			galleryGraphics.endFill();
-			galleryGraphics.lineStyle(5, 0xFFFFFF, 1);
-			galleryGraphics.drawPolygon(level.holes[i].points);
-		};
-
-		//draw polygon points (Gallery + Holes-)
-		pointgraphics.lineStyle(1, 0x000000, 1);
-		pointgraphics.beginFill(0x000000);
-		for (var i = 0; i < level.gallery.points.length; i+=2) {
-			pointgraphics.drawCircle(level.gallery.points[i], level.gallery.points[i+1], 5);
-		};
-		for (var i = 0; i < level.holes.length; i++) {
-			for (var j = 0; j < level.holes[i].points.length; j+=2) {
-				pointgraphics.drawCircle(level.holes[i].points[j], level.holes[i].points[j+1], 5);
-			};
-		};
-		pointgraphics.endFill();
-
-		//draw Guards
-		guardGraphics.lineStyle(1, 0x000000, 1);
-		guardGraphics.beginFill(0xff0000);
-		for (var i = 0; i < level.guards.length; i++) {
-			guardGraphics.drawCircle(level.guards[i].position.x, level.guards[i].position.y, 10);
-		};
-		//draw Player
-		guardGraphics.lineStyle(1, 0x000000, 1);
-		guardGraphics.beginFill(0x0000ff);
-		guardGraphics.drawCircle(level.player.x, level.player.y, 10);
-		guardGraphics.endFill();
+		redraw();
 	}	
+}
+
+function redraw()
+{
+	//Clear all graphics
+	galleryMask.clear();
+	wallgraphics.clear();
+	pointgraphics.clear();
+	galleryGraphics.clear();
+	guardGraphics.clear();
+	patrolGraphics.clear();
+
+	//draw Gallery
+	galleryMask.beginFill(0);
+	galleryMask.drawPolygon(level.gallery.points);
+	galleryMask.endFill();
+	wallgraphics.lineStyle(5, 0xFFFFFF, 1);
+	wallgraphics.drawPolygon(level.gallery.points);
+
+	//draw Holes
+	for (var i = 0; i < level.holes.length; i++) {
+		galleryGraphics.beginFill(0xCCCCCC);
+		galleryGraphics.drawPolygon(level.holes[i].points);
+		galleryGraphics.endFill();
+		galleryGraphics.lineStyle(5, 0xFFFFFF, 1);
+		galleryGraphics.drawPolygon(level.holes[i].points);
+	};
+
+	//draw polygon points (Gallery + Holes-)
+	pointgraphics.lineStyle(1, 0x000000, 1);
+	pointgraphics.beginFill(0x000000);
+	for (var i = 0; i < level.gallery.points.length; i+=2) {
+		pointgraphics.drawCircle(level.gallery.points[i], level.gallery.points[i+1], 5);
+	};
+	for (var i = 0; i < level.holes.length; i++) {
+		for (var j = 0; j < level.holes[i].points.length; j+=2) {
+			pointgraphics.drawCircle(level.holes[i].points[j], level.holes[i].points[j+1], 5);
+		};
+	};
+	pointgraphics.endFill();
+
+	//draw Guards
+	guardGraphics.lineStyle(1, 0x000000, 1);
+	guardGraphics.beginFill(0xff0000);
+	for (var i = 0; i < level.guards.length; i++) {
+		guardGraphics.drawCircle(level.guards[i].position.x, level.guards[i].position.y, 10);
+	};
+	//draw Player
+	guardGraphics.lineStyle(1, 0x000000, 1);
+	guardGraphics.beginFill(0x0000ff);
+	guardGraphics.drawCircle(level.player.x, level.player.y, 10);
+	guardGraphics.endFill();
+
+	//draw guard path
+	if((createpath || createguards) && guardselected >= 0)
+	{
+		console.log(guardselected);
+		//patrolGraphics.beginFill(0x0000ff);
+		//patrolGraphics.drawPolygon(level.guards[guardselected].guardpath.points);
+		//patrolGraphics.endFill();
+		patrolGraphics.lineStyle(5, 0x0000FF, 1);
+		patrolGraphics.drawPolygon(level.guards[guardselected].guardpath.points);
+	}
 }
