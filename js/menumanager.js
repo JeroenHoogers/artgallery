0bb18bcd1a8dd19e050615128975e9aa1d50b8af
Niek <niek_vanhulzen@hotmail.com>
2016-01-25 12:17:20 +0100

    Menu manager

--- js/menumanager.js
+++ js/menumanager.js
@@ -1,17 +1,23 @@
 
 var interactive = true;
 
-var initialmenu = new PIXI.Container();
+var homemenu = new PIXI.Container();
 var gameoverMenu = new PIXI.Container();
 var pauseMenu = new PIXI.Container();
 var helpMenu = new PIXI.Container();
 
 var menuGraphics = new PIXI.Graphics();
-var startBtn = new PIXI.Graphics();
+var gameBtn = new PIXI.Graphics();
 var infoBtn = new PIXI.Graphics();
-var backBtn = new PIXI.Graphics();
+var resumeBtn = new PIXI.Graphics();
+var menuTitleText = new PIXI.Text("", {font:"35px Goudy Old Style", fill:"white", stroke:"#999999", strokeThickness: 2});
+var infoText = new PIXI.Text("", {font:"25px Arial", fill:"white", stroke:"#999999", strokeThickness: 2});	
+var gameBtnText = new PIXI.Text("", {font:"25px Arial", fill:"white", stroke:"#999999", strokeThickness: 2});	
+var infoBtnText = new PIXI.Text("", {font:"25px Arial", fill:"white", stroke:"#999999", strokeThickness: 2});	
 
+var menuOpen = false;
 var menupauzeactive = false;
+var menuinfoactive = false;
 var menustartactive = false;
 var menugemeoveractive = false;
 //var btnsprite = new PIXI.Sprite(buttonTexture);
@@ -21,126 +27,121 @@ var menugemeoveractive = false;
 // text.position.x = (0 - sprite.width) + 20;
 // text.position.y = top + 20;
 
-function showstartmenu()
+function showmenu()
 {
-	menustartactive = true;
-	titleText = new PIXI.Text("Art Gallery Heist", {font:"25px Goudy Old Style", fill:"white", stroke:"#999999", strokeThickness: 2});
-	titleText.position = new PIXI.Point(0, 0);
-	startBtn.beginFill(0xFFFF00);	 	
-	startBtn.lineStyle(5, 0xFF0000);	 	
-	startBtn.drawRect(300, 100, 250, 50);		
-	startBtnText = new PIXI.Text("Start Game", {font:"25px Goudy Old Style", fill:"black", stroke:"#000000", strokeThickness: 2});
-	startBtnText.position = new PIXI.Point(310, 110);
-	startBtn.addChild(startBtnText);
-	startBtn.hitArea = startBtn.getBounds();
-	startBtn.interactive = true;	
-	infoBtn.beginFill(0xFFFF00);	
-	infoBtn.lineStyle(5, 0xFF0000);	 	
-	infoBtn.drawRect(300, 200, 250, 50);		
-	infoBtnText = new PIXI.Text("Info", {font:"25px Goudy Old Style", fill:"black", stroke:"#000000", strokeThickness: 2});
-	infoBtnText.position = new PIXI.Point(310, 210);		
+	menuOpen = true;
+	menuGraphics.clear();	
+	menuGraphics.beginFill(0x000000, 0.7);	
+	menuGraphics.lineStyle(2, 0xCCCCCC);	 
+	menuGraphics.drawRect(200, 100 , 850, 500);
+	menuTitleText.position = new PIXI.Point(500, 200);
+	gameBtn.beginFill(0x888888);	 	
+	gameBtn.lineStyle(2, 0xCCCCCC);	 
+	gameBtn.drawRect(500, 350, 250, 50);	
+	gameBtnText.position = new PIXI.Point(510, 360);		
+	gameBtn.addChild(gameBtnText);
+	gameBtn.hitArea = gameBtn.getBounds();
+	gameBtn.interactive = true;
+	infoBtn.beginFill(0x888888);	
+	infoBtn.lineStyle(2, 0xCCCCCC);	 	
+	infoBtn.drawRect(500, 500, 250, 50);	
+	infoBtnText.position = new PIXI.Point(510, 510);		
 	infoBtn.addChild(infoBtnText);
 	infoBtn.hitArea = infoBtn.getBounds();
 	infoBtn.interactive = true;
-	initialmenu.addChild(titleText);		
-	initialmenu.addChild(startBtn);	
-	initialmenu.addChild(infoBtn);
-	initialmenu.interactive = true;
-	renderer.render(initialmenu);
+	menuGraphics.addChild(menuTitleText);		
+	menuGraphics.addChild(gameBtn);		
+	menuGraphics.addChild(infoBtn);
+	homemenu.addChild(menuGraphics);
+	homemenu.interactive = true;
+	stage.addChild(homemenu);
+}
+
+function showstartmenu()
+{
+	showmenu();
+	menustartactive = true;
+	menuTitleText.text = "Art Gallery Heist";
+	gameBtnText.text = "Start Game";
+	infoBtnText.text = "Info";
+	renderer.render(stage);
 }
 
 function showpauzemenu()
 {
+	showmenu();
 	menupauzeactive = true;
-	titleText = new PIXI.Text("Paused", {font:"35px Goudy Old Style", fill:"white", stroke:"#000000", strokeThickness: 5});
-	titleText.position = new PIXI.Point(500, 200);
-	menuGraphics.lineStyle(5, 0x000000);	
-	menuGraphics.drawRect(470, 170, 300, 300);			
-	backBtn.beginFill(0xFFFF00);	 	
-	backBtn.lineStyle(5, 0xFF0000);	 
-	backBtn.drawRect(500, 400, 250, 50);			
-	infoBtnText = new PIXI.Text("Back", {font:"25px Goudy Old Style", fill:"black", stroke:"#000000", strokeThickness: 2});
-	infoBtnText.position = new PIXI.Point(510, 410);		
-	backBtn.addChild(infoBtnText);
-	backBtn.hitArea = backBtn.getBounds();
-	backBtn.interactive = true;
-	menuGraphics.addChild(titleText);		
-	menuGraphics.addChild(backBtn);
-	pauseMenu.addChild(menuGraphics);
-	pauseMenu.interactive = true;
-	stage.addChild(pauseMenu);
+	menuTitleText.text = "Paused";
+	gameBtnText.text = "Back to Start";	
+	infoBtnText.text ="Resume Game";
 	renderer.render(stage);
-	//renderer.render(pauseMenu);
 }
 
 function showgameovermenu()
 {
+	showmenu();
 	menugemeoveractive = true;
-	titleText = new PIXI.Text("Game Over", {font:"35px Goudy Old Style", fill:"white", stroke:"#000000", strokeThickness: 5});
-	titleText.position = new PIXI.Point(500, 200);
-	menuGraphics.lineStyle(5, 0x000000);	
-	menuGraphics.drawRect(470, 170, 300, 300);			
-	backBtn.beginFill(0xFFFF00);	 	
-	backBtn.lineStyle(5, 0xFF0000);	 
-	backBtn.drawRect(500, 400, 250, 50);			
-	infoBtnText = new PIXI.Text("Back", {font:"25px Goudy Old Style", fill:"black", stroke:"#000000", strokeThickness: 2});
-	infoBtnText.position = new PIXI.Point(510, 410);		
-	backBtn.addChild(infoBtnText);
-	backBtn.hitArea = backBtn.getBounds();
-	backBtn.interactive = true;
-	menuGraphics.addChild(titleText);		
-	menuGraphics.addChild(backBtn);
-	gameoverMenu.addChild(menuGraphics);
-	gameoverMenu.interactive = true;
-	stage.addChild(gameoverMenu);
+	menuTitleText.text = "Game Over";
+	gameBtnText.text = "Back to Start";	
+	infoBtnText.text = "Restart Game";		
 	renderer.render(stage);
-	//renderer.render(pauseMenu);
 }
 
-startBtn.click = function(data){
-	menustartactive = false;
-	initialize();
-	update();
-}
 
-infoBtn.click = function(data){		
-	infoText = new PIXI.Text("Art Gallery Heist \n By Jeroen Hoogers and Niek van Hulzen \n ABCDEFGHIJKLMNOPQRTSUVWXYZ", {font:"25px Goudy Old Style", fill:"white", stroke:"#999999", strokeThickness: 2});
-	infoText.position = new PIXI.Point(310, 210);	
-	backBtn.beginFill(0xFFFF00);	 	// set the line style to have a width of 5 and set the color to red	
-	backBtn.lineStyle(5, 0xFF0000);	 	// draw a rectangle	
-	backBtn.drawRect(300, 600, 250, 50);			// make the button interactive..	
-	startBtnText = new PIXI.Text("Back", {font:"25px Goudy Old Style", fill:"black", stroke:"#000000", strokeThickness: 2});
-	startBtnText.position = new PIXI.Point(310, 610);
-	backBtn.addChild(startBtnText);
-	backBtn.hitArea = backBtn.getBounds();
-	backBtn.interactive = true;	
-	initialmenu.removeChild(startBtn);
-	initialmenu.removeChild(infoBtn);
-	initialmenu.addChild(infoText);
-	initialmenu.addChild(backBtn);
-	renderer.render(initialmenu);
-}
 
-backBtn.click = function(data){		
+gameBtn.click = function(data){
 	if(menustartactive)
 	{
-		initialmenu.removeChild(infoText);
-		initialmenu.removeChild(backBtn);
-		showstartmenu();
-	}
-	else if(menupauzeactive)
-	{
-		stage.removeChild(pauseMenu);
-		menupauzeactive = false;
-		pause = false;
+		stage.removeChild(homemenu);
+		menustartactive = false;
+		currentStage = 0;
+		initialize();
 		update();
 	}
-	else if(menugemeoveractive)
+	else
 	{
-		stage.removeChild(gameoverMenu);
+		menupauzeactive = false;
 		menugemeoveractive = false;
 		menustartactive = true;
 		pause = false;
 		showstartmenu();
+	}
+}
+
+infoBtn.click = function(data){	
+	if(menustartactive)
+	{
+		showmenu();
+		menuGraphics.removeChild(gameBtn);
+		menustartactive = false;
+		menuinfoactive = true;
+		menuTitleText.text = "Game information";
+		menuTitleText.position = new PIXI.Point(500, 120);
+		infoText.text = "Art Gallery Heist \n By Jeroen Hoogers and Niek van Hulzen \n ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+		infoText.position = new PIXI.Point(225, 180);
+		menuGraphics.addChild(infoText);
+		infoBtnText.text = "Back";		
+		renderer.render(stage);
+	}
+	else if(menupauzeactive)
+	{
+		menupauzeactive = false;
+		pause = false;
+		stage.removeChild(homemenu);
+		update();
+	}
+	else if(menuinfoactive)
+	{
+		menuGraphics.removeChild(infoText);
+		menuinfoactive = false;
+		showstartmenu();
+	}
+	else if(menugemeoveractive)
+	{
+		stage.removeChild(homemenu);
+		menugemeoveractive = false;
+		currentStage = 0;
+		initialize();
+		update();
 	}
 }
